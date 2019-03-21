c28c282be04eb8ff3b59115b18c1a0ead16570d4
minoriitosu <itosu_0903@yahoo.co.jp>
2019-03-22 02:06:53 +0900

    Add Asynchronous
    What メッセージ送信機能を非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,44 @@
+$(function() {
+  function buildHTML(message) {
+    var image = message.image.url ? `<img src='${message.image.url}'> ` : ''
+    var html = `<div class = "message" data-id=${message.id}>
+                  <div class = "upper-message">
+                    <div class = "upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class = "upper-message__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <p class = "lower-message__content">
+                    ${message.content}
+                    ${image}
+                  </p>
+                </div>`
+    return html;
+  }
+  $('#new_message').submit(function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+         var href = window.location.href
+        $.ajax({
+        url: href,
+        type: "POST",
+        data: formData,
+        dataType: "json",
+        processData: false,
+        contentType: false
+        })
+            .done(function (data) {
+                var html = buildHTML(data);
+                $('.messages').append(html)
+                $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'slow');
+                $('#message_content,#message_image').val('')
+                $('.form__submit').prop('disabled', false);
+            })
+            .fail(function () {
+                alert('error')
+                $('.form__submit').prop('disabled', false);
+                      })
+    });
+ });
