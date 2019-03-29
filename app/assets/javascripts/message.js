615d05ad72d43f1419278e568760a2b73f93cc79
minoriitosu <itosu_0903@yahoo.co.jp>
2019-03-29 23:19:44 +0900

    インデント修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,47 +1,46 @@
 $(function() {
   function buildHTML(message) {
     var image = message.image.url ? `<img src='${message.image.url}'> ` : ''
-    var html = `
-    <div class = "message" data-id=${message.id}>
-    <div class = "upper-message">
-    <div class = "upper-message__user-name">
-    ${message.user_name}
-    </div>
-    <div class = "upper-message__date">
-    ${message.created_at}
-    </div>
-    </div>
-    <p class = "lower-message__content">
-    ${message.content}
-    </p>
-    <div class = "lower-message__image">
-    ${image}
-    </div>`
+    var html = `<div class = "message" data-id=${message.id}>
+                  <div class = "upper-message">
+                    <div class = "upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class = "upper-message__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <p class = "lower-message__content">
+                    ${message.content}
+                  </p>
+                  <div class = "lower-message__image">
+                  ${image}
+                </div>`
     return html;
   }
   $('#new_message').submit(function(e){
       e.preventDefault();
       var formData = new FormData(this);
-      var href = window.location.href
-      $.ajax({
-      url: href,
-      type: "POST",
-      data: formData,
-      dataType: "json",
-      processData: false,
-      contentType: false
-    })
-      .done(function (data) {
-        var html = buildHTML(data);
-        $('.messages').append(html)
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'slow');
-        $('#message_content,#message_image').val('')
-        $('.form__submit').removeAttr("disabled");
-     })
-      .fail(function () {
-        alert('error')
-          $('.form__submit').removeAttr("disabled");
+         var href = window.location.href
+        $.ajax({
+        url: href,
+        type: "POST",
+        data: formData,
+        dataType: "json",
+        processData: false,
+        contentType: false
         })
+            .done(function (data) {
+                var html = buildHTML(data);
+                $('.messages').append(html)
+                $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'slow');
+                $('#message_content,#message_image').val('')
+                $('.form__submit').removeAttr("disabled");
+            })
+            .fail(function () {
+                alert('error')
+                $('.form__submit').prop('disabled', false);
+                      })
     });
 
   var interval = setInterval(function() {
